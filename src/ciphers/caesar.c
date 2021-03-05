ca37455ba1f1584061f683126afdaa02aa493d88
jacktoh88 <tohc88@outlook.com>
2021-03-05 20:09:24 +0800

    Implemented Caesar cipher

--- src/ciphers/caesar.c
+++ src/ciphers/caesar.c
@@ -2,9 +2,35 @@
 #include <ciphers.h>
 
 bool encrypt_caesar(cipher_t *cipher){
+    // The key for the caesar cipher is just one byte maximum
+    uint8_t key = *(cipher->key);
+    if(cipher->key_size != 1){
+        return false;
+    }
+    else if (cipher->input_size != cipher->output_size){
+        return false;
+    }
+
+    for(int i = 0; i < cipher->input_size; i++){
+        cipher->output[i] = cipher->input[i] + key;
+    }
+
     return true;
 }
 
 bool decrypt_caesar(cipher_t *cipher){
+    // The key for the caesar cipher is just one byte maximum
+    uint8_t key = *(cipher->key);
+    if(cipher->key_size != 1){
+        return false;
+    }
+    else if (cipher->input_size != cipher->output_size){
+        return false;
+    }
+
+    for(int i = 0; i < cipher->input_size; i++){
+        cipher->output[i] = cipher->input[i] - key;
+    }
+
     return true;
 }
