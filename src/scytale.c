dfe331304fd71d37321e100add87e68020c61453
jacktoh88 <tohc88@outlook.com>
2021-03-05 22:01:28 +0800

    Adding file reading for input and key files

--- src/scytale.c
+++ src/scytale.c
@@ -1,10 +1,18 @@
 
 #include <stdio.h>
 #include <argp.h>
+#include <string.h>
 #include "ciphers.h"
+
+#define MAX_KEY_LEN 1024
+#define MAX_INPUT_LEN 4096
 
 const static char doc[] = "scytale is a command line tool for having fun with ancient cryptography algorithms";
 const static char args_doc[] = "";
+
+char input_filename[100] = {0};
+char output_filename[100] = {0};
+char key_filename[100] = {0};
 
 cipher_t cipher;
 
@@ -62,6 +70,15 @@ error_t parse_opt(int key, char *arg, struct argp_state *state) {
             break;
         // Path to key file
         case 'k':
+            strncpy(key_filename, arg, sizeof(key_filename));
+            break;
+        // Path to input file
+        case 'i':
+            strncpy(input_filename, arg, sizeof(input_filename));
+            break;
+        // Path to output file
+        case 'o':
+            strncpy(output_filename, arg, sizeof(output_filename));
             break;
         default:
             return ARGP_ERR_UNKNOWN;
@@ -81,4 +98,33 @@ struct argp args = {
 
 int main(int argc, char* argv[]){
     argp_parse(&args, argc, argv, 0, 0, NULL);
+    // Read files into cipher struct
+    FILE* key_fp = fopen(key_filename, "r");
+    if(!key_fp){
+        printf("Key file not found\n");
+        return 1;
+    }
+
+    uint8_t key_data[MAX_KEY_LEN];
+    cipher.key_size = fread(key_data, 1, MAX_KEY_LEN, key_fp);
+    if(cipher.key_size == 0){
+        printf("Error reading from key file\n");
+    }
+    cipher.key = key_data;
+
+    FILE* input_fp = fopen(input_filename, "r");
+    if(!input_fp){
+        printf("Input file not found\n");
+        return 1;
+    }
+
+    uint8_t input_data[MAX_INPUT_LEN];
+    cipher.input_size = fread(input_data, 1, MAX_INPUT_LEN, input_fp);
+    if(cipher.input_size == 0){
+        printf("Error reading from input file\n");
+    }
+    cipher.input = input_data;
+
+    fclose(key_filename);
+
 }
