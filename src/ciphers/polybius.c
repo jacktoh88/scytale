7a9011cafdbb9e76809c640c2cee99b2f4cd861e
jacktoh88 <tohc88@outlook.com>
2021-03-08 02:35:30 +0800

    Created encryption and decryption grids for polybius cipher

--- src/ciphers/polybius.c
+++ src/ciphers/polybius.c
@@ -1,5 +1,29 @@
 #include "ciphers.h"
 
+// Encryption grid
+// Row is stored in the first index
+// Column is stored in the second index
+uint8_t encryption_lookup[256][2];
+
+// Decryption grid
+// Stores the plaintext given the row and column
+uint8_t decryption_lookup[16][16];
+
+void setup_encryption_lookup(){
+    for(int i = 0; i < sizeof(encryption_lookup); i++){
+        encryption_lookup[i][0] = (i / 16);
+        encryption_lookup[i][1] = i % 16;
+    }
+}
+
+void setup_decryption_lookup(){
+    for(int i = 0; i < 16; i++){
+        for(int j = 0; j < 16; j++){
+            decryption_lookup[i][j] = (i*j) + j;
+        }
+    }
+}
+
 bool encrypt_polybius(cipher_t *cipher){
     return true;
 }
