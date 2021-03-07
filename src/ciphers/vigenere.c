e5314ca855bc2e7dfd71c20e7a1690b7c0cf030b
jacktoh88 <tohc88@outlook.com>
2021-03-08 01:58:34 +0800

    Implemented vigenere cipher

--- src/ciphers/vigenere.c
+++ src/ciphers/vigenere.c
@@ -1,9 +1,35 @@
 #include "ciphers.h"
 
 bool encrypt_vigenere(cipher_t *cipher){
+    if(cipher->key_size < 1){
+        return false;
+    }
+    else if (cipher->input_size != cipher->output_size){
+        return false;
+    }
+
+    for(int i = 0; i < cipher->input_size; i++){
+        for(int j = 0; j < cipher->key_size; j++){
+            cipher->output[i] = cipher->input[i] + cipher->key[j];
+        }
+    }
+
     return true;
 }
 
 bool decrypt_vigenere(cipher_t *cipher){
+    if(cipher->key_size < 1){
+        return false;
+    }
+    else if (cipher->input_size != cipher->output_size){
+        return false;
+    }
+
+    for(int i = 0; i < cipher->input_size; i++){
+        for(int j = 0; j < cipher->key_size; j++){
+            cipher->output[i] = cipher->input[i] - cipher->key[j];
+        }
+    }
+
     return true;
 }
