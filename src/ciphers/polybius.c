558f70ff3ec39db02afdd1f58b421e3f44523196
jacktoh88 <tohc88@outlook.com>
2021-03-08 03:13:46 +0800

    Implemented polybius cipher

--- src/ciphers/polybius.c
+++ src/ciphers/polybius.c
@@ -24,10 +24,33 @@ void setup_decryption_lookup(){
     }
 }
 
+// Ignoring key for both encryption and decryption
+// Instead using precalculated key look up tables
 bool encrypt_polybius(cipher_t *cipher){
+    // Output will by 2x input because each byte is transformed into 2
+    if (cipher->input_size != cipher->output_size / 2){
+        return false;
+    }
+
+    for(int i = 0; i < cipher->output_size; i+=2){
+        cipher->output[i] = encryption_lookup[cipher->input[i/2]][0];
+        cipher->output[i+1] = encryption_lookup[cipher->input[i/2]][1];
+    }
+
     return true;
 }
 
 bool decrypt_polybius(cipher_t *cipher){
+    // Output will by 1/2 input because evyer two bytes are reduced into 1
+    if (cipher->input_size != cipher->output_size * 2){
+        return false;
+    }
+
+    for(int i = 0; i < cipher->input_size; i+=2){
+        uint8_t row = cipher->input[i];
+        uint8_t column = cipher->input[i+1];
+        cipher->output[i/2] = decryption_lookup[row][column];
+    }
+
     return true;
 }
